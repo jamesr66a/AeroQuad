925648fd851d4c3a63b8b7b5b3664de213014121
James Reed <jamesr66@vt.edu>
2014-04-10 11:31:50 -0400

    few changes

---
+++ BuildAQ32/.dep/AQMath.o.d
@@ -0,0 +1,189 @@
+objSTM32/dummy/../Libraries/AQ_Math/AQMath.o: \
+ /home/james/AeroQuad/Libraries/AQ_Math/AQMath.cpp \
+ /home/james/AeroQuad/Libraries/AQ_Math/AQMath.h \
+ ../AeroQuad32/MapleCompatibility/Arduino.h \
+ ../AeroQuad32/MapleCompatibility/WProgram.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stdint.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/stdint.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/stdlib.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/_ansi.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/newlib.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/config.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/features.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stddef.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/reent.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/_ansi.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/_types.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/_types.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/_default_types.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/lock.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/stdlib.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/alloca.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/string.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/string.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/include-fixed/math.h \
+ ../Libmaple/libmaple/wirish/wirish.h \
+ ../Libmaple/libmaple/libmaple/libmaple.h \
+ ../Libmaple/libmaple/libmaple/libmaple_types.h \
+ ../Libmaple/libmaple/libmaple/stm32.h \
+ ../Libmaple/libmaple/libmaple/util.h \
+ ../Libmaple/libmaple/libmaple/delay.h \
+ ../Libmaple/libmaple/wirish/wirish_types.h \
+ ../Libmaple/libmaple/libmaple/libmaple_types.h \
+ ../Libmaple/libmaple/libmaple/gpio.h \
+ ../Libmaple/libmaple/libmaple/gpioF2.h \
+ ../Libmaple/libmaple/libmaple/libmaple.h \
+ ../Libmaple/libmaple/libmaple/rcc.h \
+ ../Libmaple/libmaple/libmaple/rccF2.h \
+ ../Libmaple/libmaple/libmaple/timer.h \
+ ../Libmaple/libmaple/libmaple/nvic.h \
+ ../Libmaple/libmaple/libmaple/bitband.h \
+ ../Libmaple/libmaple/libmaple/adc.h ../Libmaple/libmaple/wirish/boards.h \
+ ../Libmaple/libmaple/wirish/boards/aeroquad32.h \
+ ../Libmaple/libmaple/wirish/io.h \
+ ../Libmaple/libmaple/wirish/wirish_time.h \
+ ../Libmaple/libmaple/libmaple/nvic.h \
+ ../Libmaple/libmaple/libmaple/systick.h \
+ ../Libmaple/libmaple/wirish/bits.h \
+ ../Libmaple/libmaple/wirish/bit_constants.h \
+ ../Libmaple/libmaple/wirish/pwm.h \
+ ../Libmaple/libmaple/wirish/ext_interrupts.h \
+ ../Libmaple/libmaple/wirish/wirish_debug.h \
+ ../Libmaple/libmaple/wirish/wirish_math.h \
+ ../Libmaple/libmaple/wirish/comm/HardwareSPI.h \
+ ../Libmaple/libmaple/libmaple/spi.h ../Libmaple/libmaple/libmaple/gpio.h \
+ ../Libmaple/libmaple/wirish/boards.h \
+ ../Libmaple/libmaple/wirish/comm/HardwareSerial.h \
+ ../Libmaple/libmaple/libmaple/usart.h \
+ ../Libmaple/libmaple/libmaple/ring_buffer.h \
+ ../Libmaple/libmaple/wirish/Print.h \
+ ../Libmaple/libmaple/wirish/HardwareTimer.h \
+ ../Libmaple/libmaple/wirish/usb_serial.h \
+ ../Libmaple/libmaple/wirish/Print.h
+
+/home/james/AeroQuad/Libraries/AQ_Math/AQMath.h:
+
+../AeroQuad32/MapleCompatibility/Arduino.h:
+
+../AeroQuad32/MapleCompatibility/WProgram.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stdint.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/stdint.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/stdlib.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/ieeefp.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/_ansi.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/newlib.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/config.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/features.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stddef.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/reent.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/_ansi.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/_types.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/_types.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/_default_types.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/lock.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/stdlib.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/alloca.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/string.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/string.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/include-fixed/math.h:
+
+../Libmaple/libmaple/wirish/wirish.h:
+
+../Libmaple/libmaple/libmaple/libmaple.h:
+
+../Libmaple/libmaple/libmaple/libmaple_types.h:
+
+../Libmaple/libmaple/libmaple/stm32.h:
+
+../Libmaple/libmaple/libmaple/util.h:
+
+../Libmaple/libmaple/libmaple/delay.h:
+
+../Libmaple/libmaple/wirish/wirish_types.h:
+
+../Libmaple/libmaple/libmaple/libmaple_types.h:
+
+../Libmaple/libmaple/libmaple/gpio.h:
+
+../Libmaple/libmaple/libmaple/gpioF2.h:
+
+../Libmaple/libmaple/libmaple/libmaple.h:
+
+../Libmaple/libmaple/libmaple/rcc.h:
+
+../Libmaple/libmaple/libmaple/rccF2.h:
+
+../Libmaple/libmaple/libmaple/timer.h:
+
+../Libmaple/libmaple/libmaple/nvic.h:
+
+../Libmaple/libmaple/libmaple/bitband.h:
+
+../Libmaple/libmaple/libmaple/adc.h:
+
+../Libmaple/libmaple/wirish/boards.h:
+
+../Libmaple/libmaple/wirish/boards/aeroquad32.h:
+
+../Libmaple/libmaple/wirish/io.h:
+
+../Libmaple/libmaple/wirish/wirish_time.h:
+
+../Libmaple/libmaple/libmaple/nvic.h:
+
+../Libmaple/libmaple/libmaple/systick.h:
+
+../Libmaple/libmaple/wirish/bits.h:
+
+../Libmaple/libmaple/wirish/bit_constants.h:
+
+../Libmaple/libmaple/wirish/pwm.h:
+
+../Libmaple/libmaple/wirish/ext_interrupts.h:
+
+../Libmaple/libmaple/wirish/wirish_debug.h:
+
+../Libmaple/libmaple/wirish/wirish_math.h:
+
+../Libmaple/libmaple/wirish/comm/HardwareSPI.h:
+
+../Libmaple/libmaple/libmaple/spi.h:
+
+../Libmaple/libmaple/libmaple/gpio.h:
+
+../Libmaple/libmaple/wirish/boards.h:
+
+../Libmaple/libmaple/wirish/comm/HardwareSerial.h:
+
+../Libmaple/libmaple/libmaple/usart.h:
+
+../Libmaple/libmaple/libmaple/ring_buffer.h:
+
+../Libmaple/libmaple/wirish/Print.h:
+
+../Libmaple/libmaple/wirish/HardwareTimer.h:
+
+../Libmaple/libmaple/wirish/usb_serial.h:
+
+../Libmaple/libmaple/wirish/Print.h:
---
+++ BuildAQ32/.dep/EEPROM.o.d
@@ -0,0 +1,162 @@
+objSTM32/dummy/../AeroQuad32/MapleCompatibility/EEPROM.o: \
+ /home/james/AeroQuad/AeroQuad32/MapleCompatibility/EEPROM.cpp \
+ ../Libmaple/libmaple/wirish/wirish.h \
+ ../Libmaple/libmaple/libmaple/libmaple.h \
+ ../Libmaple/libmaple/libmaple/libmaple_types.h \
+ ../Libmaple/libmaple/libmaple/stm32.h \
+ ../Libmaple/libmaple/libmaple/util.h \
+ ../Libmaple/libmaple/libmaple/delay.h \
+ ../Libmaple/libmaple/wirish/wirish_types.h \
+ ../Libmaple/libmaple/libmaple/libmaple_types.h \
+ ../Libmaple/libmaple/libmaple/gpio.h \
+ ../Libmaple/libmaple/libmaple/gpioF2.h \
+ ../Libmaple/libmaple/libmaple/libmaple.h \
+ ../Libmaple/libmaple/libmaple/rcc.h \
+ ../Libmaple/libmaple/libmaple/rccF2.h \
+ ../Libmaple/libmaple/libmaple/timer.h \
+ ../Libmaple/libmaple/libmaple/nvic.h \
+ ../Libmaple/libmaple/libmaple/bitband.h \
+ ../Libmaple/libmaple/libmaple/adc.h ../Libmaple/libmaple/wirish/boards.h \
+ ../Libmaple/libmaple/wirish/boards/aeroquad32.h \
+ ../Libmaple/libmaple/wirish/io.h \
+ ../Libmaple/libmaple/wirish/wirish_time.h \
+ ../Libmaple/libmaple/libmaple/nvic.h \
+ ../Libmaple/libmaple/libmaple/systick.h \
+ ../Libmaple/libmaple/wirish/bits.h \
+ ../Libmaple/libmaple/wirish/bit_constants.h \
+ ../Libmaple/libmaple/wirish/pwm.h \
+ ../Libmaple/libmaple/wirish/ext_interrupts.h \
+ ../Libmaple/libmaple/wirish/wirish_debug.h \
+ ../Libmaple/libmaple/wirish/wirish_math.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/include-fixed/math.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/reent.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/_ansi.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/newlib.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/config.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/features.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/_types.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/_types.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/_default_types.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/lock.h \
+ /home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stddef.h \
+ ../Libmaple/libmaple/wirish/comm/HardwareSPI.h \
+ ../Libmaple/libmaple/libmaple/spi.h ../Libmaple/libmaple/libmaple/gpio.h \
+ ../Libmaple/libmaple/wirish/boards.h \
+ ../Libmaple/libmaple/wirish/comm/HardwareSerial.h \
+ ../Libmaple/libmaple/libmaple/usart.h \
+ ../Libmaple/libmaple/libmaple/ring_buffer.h \
+ ../Libmaple/libmaple/wirish/Print.h \
+ ../Libmaple/libmaple/wirish/HardwareTimer.h \
+ ../Libmaple/libmaple/wirish/usb_serial.h \
+ ../Libmaple/libmaple/wirish/Print.h \
+ /home/james/AeroQuad/AeroQuad32/MapleCompatibility/EEPROM.h \
+ /home/james/AeroQuad/AeroQuad32/MapleCompatibility/flash_stm32.h
+
+../Libmaple/libmaple/wirish/wirish.h:
+
+../Libmaple/libmaple/libmaple/libmaple.h:
+
+../Libmaple/libmaple/libmaple/libmaple_types.h:
+
+../Libmaple/libmaple/libmaple/stm32.h:
+
+../Libmaple/libmaple/libmaple/util.h:
+
+../Libmaple/libmaple/libmaple/delay.h:
+
+../Libmaple/libmaple/wirish/wirish_types.h:
+
+../Libmaple/libmaple/libmaple/libmaple_types.h:
+
+../Libmaple/libmaple/libmaple/gpio.h:
+
+../Libmaple/libmaple/libmaple/gpioF2.h:
+
+../Libmaple/libmaple/libmaple/libmaple.h:
+
+../Libmaple/libmaple/libmaple/rcc.h:
+
+../Libmaple/libmaple/libmaple/rccF2.h:
+
+../Libmaple/libmaple/libmaple/timer.h:
+
+../Libmaple/libmaple/libmaple/nvic.h:
+
+../Libmaple/libmaple/libmaple/bitband.h:
+
+../Libmaple/libmaple/libmaple/adc.h:
+
+../Libmaple/libmaple/wirish/boards.h:
+
+../Libmaple/libmaple/wirish/boards/aeroquad32.h:
+
+../Libmaple/libmaple/wirish/io.h:
+
+../Libmaple/libmaple/wirish/wirish_time.h:
+
+../Libmaple/libmaple/libmaple/nvic.h:
+
+../Libmaple/libmaple/libmaple/systick.h:
+
+../Libmaple/libmaple/wirish/bits.h:
+
+../Libmaple/libmaple/wirish/bit_constants.h:
+
+../Libmaple/libmaple/wirish/pwm.h:
+
+../Libmaple/libmaple/wirish/ext_interrupts.h:
+
+../Libmaple/libmaple/wirish/wirish_debug.h:
+
+../Libmaple/libmaple/wirish/wirish_math.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/include-fixed/math.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/reent.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/_ansi.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/newlib.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/config.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/ieeefp.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/features.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/_types.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/_types.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/machine/_default_types.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/sys/lock.h:
+
+/home/james/arm-2011.09/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stddef.h:
+
+../Libmaple/libmaple/wirish/comm/HardwareSPI.h:
+
+../Libmaple/libmaple/libmaple/spi.h:
+
+../Libmaple/libmaple/libmaple/gpio.h:
+
+../Libmaple/libmaple/wirish/boards.h:
+
+../Libmaple/libmaple/wirish/comm/HardwareSerial.h:
+
+../Libmaple/libmaple/libmaple/usart.h:
+
+../Libmaple/libmaple/libmaple/ring_buffer.h:
+
+../Libmaple/libmaple/wirish/Print.h:
+
+../Libmaple/libmaple/wirish/HardwareTimer.h:
+
+../Libmaple/libmaple/wirish/usb_serial.h:
+
+../Libmaple/libmaple/wirish/Print.h:
+
+/home/james/AeroQuad/AeroQuad32/MapleCompatibility/EEPROM.h:
+
+/home/james/AeroQuad/AeroQuad32/MapleCompatibility/flash_stm32.h:
