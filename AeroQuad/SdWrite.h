c5beb9cba0d2728eb77b6ad1862759797b75be63
gajarmin <gajarmin@gmail.com>
2014-05-08 17:39:58 -0400

    Update SdWrite.h

--- AeroQuad/SdWrite.h
+++ AeroQuad/SdWrite.h
@@ -1,8 +1,8 @@
 #ifndef SD_WRITE_H
 #define SD_WRITE_H
 
-#include "AeroQuad/Libmaple/libraries/mapleSDfat/SdFat.h"
-#include "AeroQuad/Libmaple/libraries/mapleSDfat/SdFatUtil.h"
+#include "../../Libmaple/libraries/mapleSDfat/SdFat.h"
+#include "../../Libmaple/libraries/mapleSDfat/SdFatUtil.h"
 
 //writes CR and LF to a file 
 void writeCRLF(SdFile& f){
